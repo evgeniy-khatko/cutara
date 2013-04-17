497af5f6332e7982939848c942398901cf71aa03
ekhatko <ekhatko@at-consulting.ru>
2013-04-17 11:30:58 +0400

    features for self test

---
+++ features/pages/test_table_page.rb
@@ -0,0 +1,5 @@
+# -*- encoding : utf-8 -*-
+PageObjectWrapper.define_page(:test_page_with_table) do
+  locator 'http://www.cs.tut.fi/cgi-bin/run/~jkorpela/echo.cgi?hidden+field=42&foo=bar&text=Default+text.&textarea=Default+text.%0D%0A&radio=2&checkbox2=on&select1=two+%28default%29&select2=two+%28default%29'
+  uniq_h1 :text => 'Echoing submitted form data'
+end
