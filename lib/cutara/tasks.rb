74c8d60b8d2c9e3b6998b58a2a8937c6574e569a
ekhatko <ekhatko@at-consulting.ru>
2013-05-15 15:33:18 +0400

    added generate_debug to public

--- lib/cutara/tasks.rb
+++ lib/cutara/tasks.rb
@@ -4,6 +4,7 @@ Bundler.setup
 load File.dirname(__FILE__)+"/tasks/build.rake"
 load File.dirname(__FILE__)+"/tasks/exec.rake"
 load File.dirname(__FILE__)+"/tasks/generate.rake"
+load File.dirname(__FILE__)+"/tasks/generate_debug.rake"
 load File.dirname(__FILE__)+"/tasks/known.rake"
 load File.dirname(__FILE__)+"/tasks/local_exec.rake"
 load File.dirname(__FILE__)+"/tasks/gui.rake"
